93b19b2cf3acd76212c71582f35697ca4c77fd48
safwan 169 <safwanhossainmridul@gmail.com>
2024-06-04 23:40:31 +0600

    eighth

--- src/componants/Dashboard/Myproducts.jsx
+++ src/componants/Dashboard/Myproducts.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
+import { TiEdit } from 'react-icons/ti';
+
 
 const Myproducts = () => {
     return (
-        <div>
-            <div className="z-10 lg:mx-10 md:mt-10 lg:mt-10 md:mx-10">
+        <div className=' mx-auto'>
+            <div className="z-10 lg:mx-10 md:mt-10 mx-auto lg:mt-10 md:mx-10">
                 <table className="table table-xs">
                     <thead>
                         <tr>
                             <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>company</th>
-                            <th>location</th>
-                            <th>Last Login</th>
-                            <th>Favorite Color</th>
+                            <th>Product Name</th>
+                            <th>Vote</th>
+                            <th>Status</th>
+                            <th></th>
+                            <th></th>
+                         
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr className=''>
                             <th></th>
                             <td>Cy Ganderton</td>
                             <td>Quality Control Specialist</td>
                             <td>Littel, Schaden and Vandervort</td>
-                            <td>Canada</td>
-                            <td>12/16/2020</td>
-                            <td>Blue</td>
+                            <td className='   ' ><span className='btn text-green-400'><TiEdit size={25} /></span></td>
+                            <td className=' '><span className='btn text-red-500'> <MdDelete size={25}/></span></td>
+                         
+                          
                         </tr>
 
 
