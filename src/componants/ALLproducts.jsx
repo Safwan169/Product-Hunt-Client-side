5c75c16c50ce23ed9339dbced41e0949b7c781b6
safwan 169 <safwanhossainmridul@gmail.com>
2024-06-08 22:27:23 +0900

    sfsd

--- src/componants/ALLproducts.jsx
+++ src/componants/ALLproducts.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import Alldata from './Alldata';
 import Products from './Products';
-
+import Heading from './Dashboard/Heading';
+ 
 const ALLproducts = () => {
-    const [cart]=Alldata()
-    const data=cart.filter(d=>d.status=='pending')
+    const [cart] = Alldata()
+    const data = cart.filter(d => d.status == 'pending')
     console.log(data)
     return (
-        <div className='grid grid-cols-3'>
-            {
-                data?.map(d=><Products dataa={d}></Products>)
-            }
-            
+        <div className='mt-10'>
+            <Heading text={"All Products"}></Heading>
+             <div className='grid lg:grid-cols-3 md:grid-cols-3   mx-auto  md:px-10 lg:px-10 gap-5'>
+                {
+                    data?.map(d => <Products dataa={d}></Products>)
+                }
+
+            </div>
+
         </div>
     );
 };
