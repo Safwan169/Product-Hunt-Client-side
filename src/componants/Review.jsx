41c7c7188692b011d3fc4c8afc7a54c7977b9d73
safwan 169 <safwanhossainmridul@gmail.com>
2024-06-09 21:39:16 +0600

    uncount

--- src/componants/Review.jsx
+++ src/componants/Review.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Alldata from './Alldata';
 import { Link } from 'react-router-dom';
 import { TbListDetails } from 'react-icons/tb';
@@ -6,11 +6,56 @@ import { MdOutlineFeaturedPlayList, MdOutlineFileDownloadDone } from 'react-icon
 import Heading from './Dashboard/Heading';
 import { TiDelete } from 'react-icons/ti';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const Review = () => {
-  const [cart] = Alldata()
+
+  // const [data, setData] = useState([
+  //   { id: 1, name: "Item 1", status: "active" },
+  //   { id: 2, name: "Item 2", status: "rejected" },
+  //   { id: 3, name: "Item 3", status: "pending" },
+  //   { id: 4, name: "Item 4", status: "active" },
+  //   { id: 5, name: "Item 5", status: "pending" },
+  //   { id: 6, name: "Item 6", status: "rejected" }
+  // ]);
+
+  const [cart,refetch,] = Alldata()
+  const [Dataa,SetData]=useState([ ])
+
+
+  useEffect(() => {
+    const sortDataByStatus = (Dataa) => {
+      const statusOrder = ["pending", "Accepted", "Rejected"];
+      return Dataa.sort((a, b) => statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status));
+    };
+
+    SetData(sortDataByStatus(cart));
+  }, []);
+ 
+console.log(cart) 
+
+
+
+  // const statusOrder = ["pending", "Accepted", "Rejected"];
+  // const sortedData = cart.sort((a, b) => {
+  //   return statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status);
+  // });
+  // SetData(sortedData);
+  // console.log(cart)
   const handleAcepted=id=>{
-    axios.put(`http://localhost:5000/${id}`)
+    axios.put(`http://localhost:5000/status/${id}`)
+    .then(res=>{
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Accepted",
+        showConfirmButton: false,
+        timer: 1500
+      }),
+  
+        refetch()}
+  )
+
     // TODO:back hand ar kaj baki r baki hoilo status update koira abr saita refresh korta hoibo abr aikan ta sorai tay hoibo ai product ta abr delete ar kaj o baki .
   }
   return (
@@ -29,20 +74,24 @@ const Review = () => {
                             <th className=' '>Product Name</th>
                             <th className=' '>Details</th>
                             <th className=' '>Make Featured</th>
-                            <th className=''></th>
+                            <th className=''>Status</th>
                             <th className='w-4'></th>
+                            <th></th>
                          
                         </tr>
                     </thead>
                       {
-                        cart.map((data,index)=> <tbody> <tr className=''>
+                        Dataa?.map((data,index)=> <tbody> <tr className=''>
                         <th>{index+1}</th>
                         <td>{data.productName}</td>
                         <td className=' '><Link className='btn' to={`/details/${data._id}`}><TbListDetails size={25} className='   text-green-400' /></Link></td>
-                        {/* <td></td>    */}
                         <td className=''><span className='btn'><MdOutlineFeaturedPlayList className='text-blue-400' size={25} /></span></td>
-                        <td onClick={()=>handleAcepted(data._id)} className=''><button className=''><span className='btn '><MdOutlineFileDownloadDone size={25} className='text-green-400 ' /></span></button></td>
-                        <td><button className='btn'><TiDelete size={25} className='text-red-500' /></button></td>
+                        <td className=' text-gray-400 font-semibold'>{data.status}</td>   
+                        {data.status=="Accepted" || data.status =="Rejected" ?  <td ><button className=''><span  className=''><MdOutlineFileDownloadDone size={25} className='text-gray-400 disabled' /></span></button></td>:<td onClick={()=>handleAcepted(data._id)}  className={``}><button className=''><span  className=''><MdOutlineFileDownloadDone size={25} className='text-green-400 ' /></span></button></td>}
+                        {data.status=="Accepted" || data.status == "Rejected"? 
+                        <td><button className='btn'><TiDelete size={25} className='text-gray-400 disabled' /></button></td>
+
+                          :<td><button className='btn'><TiDelete size={25} className='text-red-500' /></button></td>}
                      
                       
                     </tr>
